3dec99690c753dcb2302366e1c01c809d90c2a5d
AGAFONOV <ivanagaf96@gmail.com>
2022-04-07 19:34:57 +0300

    Add check sum

--- src/main/java/HomeWorkApp.java
+++ src/main/java/HomeWorkApp.java
@@ -1,11 +1,28 @@
 public class HomeWorkApp {
     public static void main(String[] args) {
         printThreeWords();
+        checkSumSign();
     }
 
     public static void  printThreeWords(){
+        System.out.println("Упражнение 2:");
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
+    }
+
+    public static void checkSumSign(){
+        int a = 10;
+        int b = 1;
+        int c = a+b;
+
+        System.out.println("Упражнение 3:");
+
+        if (c>=0){
+            System.out.println("Сумма положительная");
+        }
+        else {
+            System.out.println("Сумма отрицательная");
+        }
     }
 }
