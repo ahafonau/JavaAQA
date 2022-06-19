2e43b3eb86497d15080f1872715b909a498ae49c
AGAFONOV <ivanagaf96@gmail.com>
2022-06-19 17:45:52 +0300

    Lesson 14 done

--- src/main/java/lesson_6/MyArraySizeException.java
+++
@@ -1,7 +0,0 @@
-package lesson_6;
-
-public class MyArraySizeException extends Exception {
-    public MyArraySizeException() {
-        super("Array size is different");
-    }
-}
---
+++ src/test/java/lesson_14/TestData.java
@@ -0,0 +1,9 @@
+package lesson_14;
+
+public class TestData {
+    public static final String URL = "https://andersenlab.com/";
+    public static final String ANDERSEN_LOGO_XPATH = "//a/*[1]";
+    public static final String ANDERSEN_MAIL = "//a[@href='mailto:vn@andersenlab.com']";
+    public static final String EXPECTED_ANDERSEN_LOGO_CLASS = "Header-module--logo--7bOtT Header-module--transparentLogo--PtA+p";
+    public static final String EXPECTED_ANDERSEN_MAIL_CLASS = "Contacts-module--link--EsZZy";
+}
